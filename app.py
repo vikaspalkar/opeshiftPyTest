dd6c63556c1980974ff0c5b75f6da79eb6751ce0
Vikas Palkar <vikaspalkar@users.noreply.github.com>
2018-09-23 21:58:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-#This is a temporary script file.
+#This is a temporary script fil
+
+import MySQLdb
+import os
+
+print("This is a simple Python MySQL Connectivity code")
+username = os.environ['MYSQL_USER']
+password = os.environ['MYSQL_ROOT_PASSWORD']
+print("Running with user: %s" % username)
+# Open database connection
+
+db = MySQLdb.connect("172.30.88.85","username","password","sampledb" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# execute SQL query using execute() method.
+cursor.execute("SELECT VERSION()")
+
+# Fetch a single row using fetchone() method.
+data = cursor.fetchone()
+print "Database version : %s " % data
+
+sql = "SELECT * FROM EMP;"
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      emp_id = row[0]
+      name = row[1]
+      sal = row[2]
+      
+      # Now print fetched result
+      print "emp_id=%d,name=%s,sal=%s \
+             (emp_id, name, sal )
+except:
+   print "Error: unable to fecth data"
 
 
+# disconnect from server
+db.close()
 
-print("Hello World")
-
-print("This is a simple Python code")
