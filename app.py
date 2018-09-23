a0f9e400b37d661e7b9421c4dca0c64785734d97
Vikas Palkar <vikaspalkar@users.noreply.github.com>
2018-09-23 22:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,10 @@
 #This is a temporary script fil
 
-import MySQLdb
+
 import os
 
-print("This is a simple Python MySQL Connectivity code")
+print("This is a simple Python....!")
 username = os.environ['MYSQL_USER']
 password = os.environ['MYSQL_ROOT_PASSWORD']
 print("Running with user: ",username)
-# Open database connection
-
-db = MySQLdb.connect("172.30.88.85","username","password","sampledb" )
-
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-
-# execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
-
-# Fetch a single row using fetchone() method.
-data = cursor.fetchone()
-print ("Database version : ", data)
-
-sql = "SELECT * FROM EMP;"
-try:
-   # Execute the SQL command
-   cursor.execute(sql)
-   # Fetch all the rows in a list of lists.
-   results = cursor.fetchall()
-   for row in results:
-      emp_id = row[0]
-      name = row[1]
-      sal = row[2]
-      
-      # Now print fetched result
-      print (emp_id, name, sal )
-except:
-   print ("Error: unable to fecth data")
-
-
-# disconnect from server
-db.close()
-
+print("Password is: ",password)
